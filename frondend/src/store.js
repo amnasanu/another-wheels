09bf1077c3b11467071845bea063e21142119300
amnasanu <amnasanu100@gmail.com>
2022-11-10 18:18:34 +0530

    add newconstrain

--- frondend/src/store.js
+++ frondend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import thunk from 'redux-thunk'
 import { productListReducers, productDetailsReducer } from './reducers/productReducers'
+import { cartReducers } from './reducers/cartReducers'
 
 
 const reducer = combineReducers({
     productList :productListReducers,
     productDetails :productDetailsReducer,
+    cart :cartReducers,
 });
 const initialState = {};
 
