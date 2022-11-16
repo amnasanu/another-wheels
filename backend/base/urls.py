11ddd6f0cc8bc74f5c822ace1b812215acb00d88
amnasanu <amnasanu100@gmail.com>
2022-11-16 15:41:16 +0530

    update JWT

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+     path('users/profile/',views.getUserProfile,name="users-profile"),
      path('users/login',views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
      path('',views.getRoutes,name="routes"),
      path('products',views.getProducts,name="routes"),
