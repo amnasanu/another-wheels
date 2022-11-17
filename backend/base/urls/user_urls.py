8d86dcfa2f73fd82515c232060493d11bb71f5a0
amnasanu <amnasanu100@gmail.com>
2022-11-17 22:02:59 +0530

    add profile updation serializer

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -3,6 +3,7 @@ from base.views import user_views as views
 
 urlpatterns=[
      path('profile/',views.getUserProfile,name="users-profile"),
+     path('profile/update/',views.updateUserProfile,name="users-profile-update"),
      path('register/',views.registerUser, name='register'),
      path('login',views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
      path('',views.getUsers,name="users"),
