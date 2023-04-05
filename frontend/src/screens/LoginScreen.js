c7f4371a4daf4ba312f67ccf9dff6dcf30de6031
amnasanu <amnasanu100@gmail.com>
2023-04-05 14:18:04 -0700

    change login form

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,12 +1,13 @@
-import React, {useState, useEffect} from 'react'
-import { Link, useLocation ,useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import Loader from '../components/Loadar'
-import Message from '../components/Messages'
-import FormContainer from '../components/FormContainer'
-import { login } from '../actions/userAction'
+import React, {useState, useEffect} from 'react';
+import { Link, useLocation ,useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Button, Row, Col, Card } from 'react-bootstrap';
+import Loader from '../components/Loadar';
+import Message from '../components/Messages';
+import FormContainer from '../components/FormContainer';
+import { login } from '../actions/userAction';
 
+import './css/LoginScreen.css';
 
 function LoginScreen({ history}) {
     const [email,setEmail] = useState('')
@@ -33,31 +34,32 @@ function LoginScreen({ history}) {
     }
     
   return (
-    <FormContainer>
-        <h1>Sign In</h1>
-        {error && <Message variant = 'danger'>{error}</Message>}
+    <div className="login-container">
+      <Card className="login-card">
+        <h2 className="text-center">Sign In</h2>
+        {error && <Message variant='danger'>{error}</Message>}
         {loading && <Loader/>}
-        <Form onSubmit = {submitHandler}>
-            <Form.Group controlId = 'email'>
-                       <Form.Label>Email Address</Form.Label>
-                       <Form.Control type='email' placeholder = 'Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-            </Form.Group>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+          </Form.Group>
 
-            <Form.Group controlId = 'password'>
-                       <Form.Label>Password</Form.Label>
-                       <Form.Control type='password' placeholder = 'Enter Password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-            </Form.Group>
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+          </Form.Group>
 
-            <Button type='submit' variant = 'primary'>Sign In</Button>
+          <Button type='submit' variant='primary' className="login-button">Sign In</Button>
         </Form>
         <Row className='py-3'>
-            <Col>
-             New Customer ? < Link to={redirect ? `/register?redirect=${redirect}` :'/register'}>Register</Link>
-            </Col>
-
+          <Col>
+            New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+          </Col>
         </Row>
-    </FormContainer>
+      </Card>
+    </div>
   )
 }
 
-export default LoginScreen
+export default LoginScreen;
